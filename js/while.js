ab0032c64676ec381a8750e24bbea68678f66e80
romulofull <romulo.castro@gmail.com>
2024-07-28 17:31:22 -0500

    Ejercicio con Do-While

--- js/while.js
+++ js/while.js
@@ -7,3 +7,11 @@ while (contador<10) {
 for (var i=0; i<10; i++) {
     console.log("Vuelta numero " + i);
 }
+
+var contador = 1;
+do {
+console.log ("Voy por la Vuelta Numero " + contador);
+contador ++;
+
+    }
+while (contador <= 10)
